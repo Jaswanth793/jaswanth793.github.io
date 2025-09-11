225196ac555c7830212b0d4d0a7927e475c982f3
Jaswanth VG <36354900+Jaswanth793@users.noreply.github.com>
2025-09-11 18:55:14 +0530

    Delete _scripts/google-analytics-setup.js

--- _scripts/google-analytics-setup.js
+++
@@ -1,9 +0,0 @@
----
-permalink: /assets/js/google-analytics-setup.js
----
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  window.dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "G-9410DR0XKQ");
